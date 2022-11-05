64201179e1ef4953ef376ae36952e9f88985d7b2
cosmoglint <ramprakashsmail@gmail.com>
2022-11-05 13:03:10 +0530

    FIX: rotate function bug

--- android_app/app/src/main/java/com/example/the_blind_watchmaker/WatchBoard.kt
+++ android_app/app/src/main/java/com/example/the_blind_watchmaker/WatchBoard.kt
@@ -44,7 +44,7 @@ class WatchBoard @JvmOverloads constructor(context: Context, attrs: AttributeSet
 
     private fun crateConfig(): Config {
         var drawColor = ResourcesCompat.getColor(resources, R.color.black, null)
-        return Config(this.canvasWidth, this.canvasWidth, drawColor)
+        return Config(this.canvasWidth, this.canvasWidth, drawColor, 10.toFloat(), 0.7.toFloat(), watchCanvas, 2)
     }
 
     private fun handleTouch() {
@@ -52,13 +52,13 @@ class WatchBoard @JvmOverloads constructor(context: Context, attrs: AttributeSet
         Log.d("TAG", this.canvasHeight.toString() + this.canvasWidth.toString())
         watchCanvas.drawARGB(0, 225, 225, 255);
         this.config = crateConfig()
-        var testWatch: Watch = Watch(0, 0, this.config, watchCanvas)
+        var testWatch: Watch = Watch(0, 300, this.config, watchCanvas)
         testWatch.createBranches()
         testWatch.showBranches()
     }
 }
 
-class Config constructor(val watchWidth: Int, val watchHeight: Int, val drawColor: Int){
+class Config constructor(val watchWidth: Int, val watchHeight: Int, val drawColor: Int, val tiltAngle: Float, val childScale: Float, val canvas: Canvas, val depth: Int){
     var linePaint = Paint().apply {
         color = drawColor
         style = Paint.Style.FILL
@@ -91,12 +91,12 @@ class Watch constructor(val startXPos: Int, val startYPos: Int, val config: Conf
 //        create branches and proliferate
         var branchHelper = lineHelper()
         val firstBranch: Branch = Branch(
-            branchHelper.elementAt(0).x,
-            branchHelper.elementAt(0).y,
-            branchHelper.elementAt(1).x,
-            branchHelper.elementAt(1).y,
+            branchHelper.elementAt(0),
+            branchHelper.elementAt(1),
             calcLength(branchHelper.elementAt(0), branchHelper.elementAt(1)),
-            1.7.toFloat()
+            90.toFloat(),
+            config,
+            0
         )
         this.branchList.add(firstBranch)
     }
@@ -105,7 +105,8 @@ class Watch constructor(val startXPos: Int, val startYPos: Int, val config: Conf
         Log.d("TAG", "showing watch branches")
 //        create branches and proliferate
         for (branch in branchList){
-            branch.showBranch(watchCanvas, config.linePaint)
+            branch.spawnChildren()
+            branch.showBranch()
         }
     }
 }
@@ -115,9 +116,11 @@ class Watch constructor(val startXPos: Int, val startYPos: Int, val config: Conf
 class Point constructor(val x: Float, val y: Float) {
 }
 
-class Branch constructor(val startX: Float, val startY: Float, private val endX: Float, val endY: Float, val branchLen: Float, val scale: Float){
+class Branch constructor(val startPoint: Point, private val endPoint: Point, val branchLen: Float, val angle: Float, val config: Config, val depth: Int){
+    private lateinit var childrenList: List<Branch>
 
     fun extendBranch(startX: Float, startY: Float, endX: Float, endY: Float): Point{
+        val scale = config.childScale
         val newX = endX + ( ( endX - startX ) / branchLen ) * scale
         val newY = endY + ( ( endY - startY ) / branchLen ) * scale
         Log.d("EXTENDED", newX.toString() + "     " + newY.toString())
@@ -127,28 +130,136 @@ class Branch constructor(val startX: Float, val startY: Float, private val endX:
     fun rotateChild_11(angle: Float, newX: Float, newY: Float): Point{
 //        var childEndX = k
         var angle = Math.toRadians(angle.toDouble())
-        var rotatedX = (newX - this.endX) * cos(angle.toDouble()) - ( newY - this.endY) * sin(angle.toDouble()) + this.endX
-        var rotatedY = (newX - this.endX) * sin(angle.toDouble()) - ( newY - this.endY) * cos(angle.toDouble()) + this.endY
+        var rotatedX = (newX - this.endPoint.x) * cos(angle.toDouble()) - ( newY - this.endPoint.y) * sin(angle.toDouble()) + this.endPoint.x
+        var rotatedY = (newX - this.endPoint.x) * sin(angle.toDouble()) - ( newY - this.endPoint.y) * cos(angle.toDouble()) + this.endPoint.y
         return Point(rotatedX.toFloat(), rotatedY.toFloat())
     }
 
+    fun quadrantConvertor(angle: Float): Array<Int> {
+        // IMPORTANT: since the y axis is inverted on the canvas quadrants will change appropriately
+        // basically the current angles mentioned are for classic quadrants but values are offsetted for canvas
+        var modAngle = angle % 360
+        Log.d("ANGLE", modAngle.toString() + "     " + angle.toString())
+        var sinValue = 1
+        var cosValue = 1
+        if ( ( modAngle < 90 ) && ( modAngle > 0 ) ) {
+            Log.d("CASE 1", "case 1")
+            sinValue = 1
+            cosValue = 1
+        }
+        else if ( ( modAngle < 180 ) && ( modAngle > 90 ) ) {
+            Log.d("CASE 2", "case 2")
+            sinValue = 1
+            cosValue = -1
+        }
+        else if ( ( modAngle < 270 ) && ( modAngle > 180 ) ) {
+            Log.d("CASE 3", "case 3")
+            sinValue = -1
+            cosValue = -1
+        }
+        else if ( ( modAngle < 360 ) && ( modAngle > 270 ) ) {
+            Log.d("CASE 4", "case 4")
+            sinValue = -1
+            cosValue = 1
+        }
+        else{
+            sinValue = 1
+            cosValue = 1
+        }
+        Log.d("LOGGING", sinValue.toString() + "       " + cosValue.toString())
+        return arrayOf(sinValue, cosValue)
+    }
+
     fun rotateChild(length: Float, angle: Float): Point{
-        var xPos = length * cos(Math.toRadians(angle.toDouble()))
-        var yPos = length * sin(Math.toRadians(angle.toDouble()))
-        Log.d("ROTATE", Math.toRadians(angle.toDouble()).toString())
+        var finAngle = 360 - angle
+        var valueArray = quadrantConvertor(angle)
+        var sinValue = valueArray.elementAt(0)
+        var cosValue = valueArray.elementAt(1)
+        var yPos = length * sin(Math.toRadians(finAngle.toDouble()))
+        var xPos = length * cos(Math.toRadians(finAngle.toDouble()))
+        Log.d("ROTATE", Math.toRadians(finAngle.toDouble()).toString())
         Log.d("ROTATED VALUES", xPos.toString() + "            " + yPos.toString() )
         return Point(xPos.toFloat(), yPos.toFloat())
     }
 
-    fun moveToOrigin(originalPoint: Point): Point{
-        return Point(originalPoint.x + startX, originalPoint.y + startY)
+    fun spawnChildren(){
+        var childLen = branchLen * config.childScale
+        Log.d("CHILD LEN", childLen.toString())
+        var rightPoint = moveToOrigin(rotateChild(childLen, this.angle + config.tiltAngle))
+//        val ec = rc)
+        var leftPoint = moveToOrigin(rotateChild(childLen, this.angle - config.tiltAngle))
+        Log.d("POINTS", rightPoint.x.toString() + "            " + rightPoint.y.toString() )
+        Log.d("POINTS", leftPoint.x.toString() + "            " + leftPoint.y.toString() )
+        var rightChild = Branch(
+            endPoint,
+            rightPoint,
+            childLen,
+            (angle + config.tiltAngle).toFloat(),
+            config,
+            depth + 1
+        )
+        var leftChild = Branch(
+            endPoint,
+            leftPoint,
+            childLen,
+            (angle - config.tiltAngle).toFloat(),
+            config,
+            depth + 1
+        )
+        childrenList = listOf(leftChild, rightChild)
+//        var leftChild = Branch(
+//            branchHelper.elementAt(0),
+//            branchHelper.elementAt(1),
+//            calcLength(branchHelper.elementAt(0), branchHelper.elementAt(1)),
+//            1.7.toFloat(),
+//            180.toFloat()
+//        )
     }
 
-    fun showBranch(canvas: Canvas, paint: Paint){
+    fun moveToOrigin(originalPoint: Point): Point{
+        return Point(originalPoint.x + startPoint.x, originalPoint.y + startPoint.y)
+    }
+
+    fun drawLine(size: Int, angle: Int){
+        var canvas = config.canvas
+        var paint = config.linePaint
+        var testChild = moveToOrigin(rotateChild(size.toFloat(), angle.toFloat()))
+        canvas.drawLine(startPoint.x, startPoint.y, testChild.x, testChild.y, paint)
+        canvas.drawCircle(testChild.x, testChild.y, 10.toFloat(), paint);
+    }
+
+    fun showBranch(){
+        var canvas = config.canvas
+        var paint = config.linePaint
         Log.d("TAG", "rendering branches")
-        val rc = rotateChild(branchLen, 0.toFloat())
-        val ec = moveToOrigin(rc)
-        canvas.drawLine(startX, startY, rc.x, rc.y, paint)
+//        val rc = rotateChild(branchLen, this.angle.toFloat())
+//        val ec = moveToOrigin(rc)
+//        canvas.drawLine(startPoint.x, startPoint.y, rc.x, rc.y, paint)
+        canvas.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y, paint)
+//        drawLine(100, 0)
+//        drawLine(100, 30)
+//        drawLine(100, 60)
+//        drawLine(100, 90)
+//        drawLine(100, 110)
+//        drawLine(100, 130)
+//        drawLine(100, 310)
+//        drawLine(100, 220)
+//        drawLine(100, 270)
+//        drawLine(100, 300)
+//        drawLine(100, 320)
+        canvas.drawCircle(endPoint.x, endPoint.y, 10.toFloat(), paint);
+        var leftChild = childrenList.elementAt(0)
+        var rightChild = childrenList.elementAt(1)
+        if (depth == config.depth){
+            return
+        }
+        else{
+            leftChild.spawnChildren()
+            leftChild.showBranch()
+            rightChild.spawnChildren()
+            rightChild.showBranch()
+        }
+
     }
 
 }
